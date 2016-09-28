fc2ff4b5cd81f94f115fcf98d2a19ca56e272f7f
George Dobrovolsky <georgy.dobrovolsky@gmail.com>
2016-09-28 21:18:26 +0300

    Fix formatting (not an error). Task 3 complete.

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
-import sys , random
-def compute ( n ):
+import sys, random
+def compute (n):
     i = 0; s = 0
     while i <= n:
-        s += random . random ()
+        s += random.random()
         i += 1
-    return s /n
+    return s/n
 
-n = sys . argv [1]
-print ("average of %d random numbers is %g " % (n , compute (n)))
+n = sys.argv[1]
+print ("average of %d random numbers is %g" % (n , compute (n)))
